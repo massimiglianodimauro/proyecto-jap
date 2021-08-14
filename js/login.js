05ffa36b9668326f8b3af22a41d5743a9304211b
massimiglianodimauro <88333534+massimiglianodimauro@users.noreply.github.com>
2021-08-14 16:38:36 -0300

    Entrega 1

--- js/login.js
+++ js/login.js
@@ -1,6 +1,22 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {});
 
-});
+function verificar() {
+  let dato = document.getElementById("nombre");
+  let password = document.getElementById("contraseña");
+  var usuario = {};
+  if (dato.value.trim() === "") {
+    alert("Faltan datos");
+  } else {
+    location.href = "principal.html";
+    usuario.nombre = dato.value;
+    usuario.estado = "conectado";
+    usuario.password = password.value;
+
+    alert("Logueado");
+
+    localStorage.setItem("usuario", JSON.stringify(usuario));
+  }
+}
