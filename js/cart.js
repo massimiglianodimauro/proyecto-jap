fbf8a295567ef8e8018ebaa3843821214dc06e3e
massimiglianodimauro <88333534+massimiglianodimauro@users.noreply.github.com>
2021-11-14 20:32:11 -0300

    ultimos detalles

--- js/cart.js
+++ js/cart.js
@@ -159,7 +159,7 @@ function validarTarjeta() {
         metodoDePagoInfo.numeroTarjeta = document.getElementById("numeroTarjeta");
         metodoDePagoInfo.vencimientoTarjeta = document.getElementById("vencimientoTarjeta");
         metodoDePagoInfo.cvvTarjeta = document.getElementById("cvvTarjeta");
-        document.getElementById("formaSeleccionada").innerHTML = "Opción: " + metodoDePagoInfo.opcion;
+        document.getElementById("formaSeleccionada").innerHTML = "Opción seleccionada: " + metodoDePagoInfo.opcion;
 
     }
 }
@@ -173,7 +173,7 @@ function validarTransferencia() {
         metodoDePagoInfo.nombreTitularCuenta = document.getElementById("nombreTitularCuenta");
         metodoDePagoInfo.numeroCuenta = document.getElementById("numeroCuenta");
 
-        document.getElementById("formaSeleccionada").innerHTML = "Opción: " + metodoDePagoInfo.opcion;
+        document.getElementById("formaSeleccionada").innerHTML = "Opción seleccionada: " + metodoDePagoInfo.opcion;
 
     }
 }
@@ -222,3 +222,4 @@ function validar() {
         alert('ingrese correctamente su dirección')
     }
 }
+
